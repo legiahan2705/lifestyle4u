8d4742faea3ddfc33f64084ed6c479f425b09ffe
legiahan2705 <155038296+legiahan2705@users.noreply.github.com>
2023-12-28 11:04:54 +0700

    add_file_web_lifestyle4u

---
+++ index.js
@@ -0,0 +1,95 @@
+document.querySelector('.react_icon1').onclick = () => {
+    document.querySelector('.react_icon11').classList.toggle('active');
+}
+
+document.querySelector('.react_icon11').onclick = () => {
+    document.querySelector('.react_icon11').classList.remove('active');
+}
+
+document.querySelector('.react_icon1a').onclick = () => {
+    document.querySelector('.react_icon11a').classList.toggle('active');
+}
+
+document.querySelector('.react_icon11a').onclick = () => {
+    document.querySelector('.react_icon11a').classList.remove('active');
+}
+
+document.querySelector('.react_icon1b').onclick = () => {
+    document.querySelector('.react_icon11b').classList.toggle('active');
+}
+
+document.querySelector('.react_icon11b').onclick = () => {
+    document.querySelector('.react_icon11b').classList.remove('active');
+}
+
+document.querySelector('.react_icon1c').onclick = () => {
+    document.querySelector('.react_icon11c').classList.toggle('active');
+}
+
+document.querySelector('.react_icon11c').onclick = () => {
+    document.querySelector('.react_icon11c').classList.remove('active');
+}
+
+document.querySelector('.react_icon1d').onclick = () => {
+    document.querySelector('.react_icon11d').classList.toggle('active');
+}
+
+document.querySelector('.react_icon11d').onclick = () => {
+    document.querySelector('.react_icon11d').classList.remove('active');
+}
+
+document.querySelector('.react_icon1e').onclick = () => {
+    document.querySelector('.react_icon11e').classList.toggle('active');
+}
+
+document.querySelector('.react_icon11e').onclick = () => {
+    document.querySelector('.react_icon11e').classList.remove('active');
+}
+
+document.querySelector('.react_icon1f').onclick = () => {
+    document.querySelector('.react_icon11f').classList.toggle('active');
+}
+
+document.querySelector('.react_icon11f').onclick = () => {
+    document.querySelector('.react_icon11f').classList.remove('active');
+}
+
+document.querySelector('.react_icon1g').onclick = () => {
+    document.querySelector('.react_icon11g').classList.toggle('active');
+}
+
+document.querySelector('.react_icon11g').onclick = () => {
+    document.querySelector('.react_icon11g').classList.remove('active');
+}
+
+document.querySelector('.react_icon1h').onclick = () => {
+    document.querySelector('.react_icon11h').classList.toggle('active');
+}
+
+document.querySelector('.react_icon11h').onclick = () => {
+    document.querySelector('.react_icon11h').classList.remove('active');
+}
+
+document.querySelector('.react_icon1i').onclick = () => {
+    document.querySelector('.react_icon11i').classList.toggle('active');
+}
+
+document.querySelector('.react_icon11i').onclick = () => {
+    document.querySelector('.react_icon11i').classList.remove('active');
+}
+
+document.querySelector('.react_icon1j').onclick = () => {
+    document.querySelector('.react_icon11j').classList.toggle('active');
+}
+
+document.querySelector('.react_icon11j').onclick = () => {
+    document.querySelector('.react_icon11j').classList.remove('active');
+}
+
+document.querySelector('.react_icon1k').onclick = () => {
+    document.querySelector('.react_icon11k').classList.toggle('active');
+}
+
+document.querySelector('.react_icon11k').onclick = () => {
+    document.querySelector('.react_icon11k').classList.remove('active');
+}
