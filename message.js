1686adc2d42c6220cab6e0dbab74dd5301b506a5
legiahan2705 <155038296+legiahan2705@users.noreply.github.com>
2023-12-29 22:23:41 +0700

    add_file_web_lifestyle4u

---
+++ message.js
@@ -0,0 +1,7 @@
+document.querySelector('.friend2').onclick = () => {
+    document.querySelector('.mess2a').classList.toggle('active');
+}
+
+document.querySelector('.friend_mess1').onclick = () => {
+    document.querySelector('.mess1').classList.toggle('active');
+}
