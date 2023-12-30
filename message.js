4c32518e0b74e63eb3d9521cf903c47b1351397d
legiahan2705 <155038296+legiahan2705@users.noreply.github.com>
2023-12-30 12:19:34 +0700

    add_file_mess

--- message.js
+++ message.js
@@ -1,7 +1,9 @@
+document.querySelector('.friend_mess1').onclick = () => {
+    document.querySelector('.mess1').classList.toggle('active');
+}
+
 document.querySelector('.friend2').onclick = () => {
     document.querySelector('.mess2a').classList.toggle('active');
 }
 
-document.querySelector('.friend_mess1').onclick = () => {
-    document.querySelector('.mess1').classList.toggle('active');
-}
+
